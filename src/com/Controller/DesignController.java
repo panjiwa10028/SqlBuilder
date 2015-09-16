6d754284b212032f41970515a8aeb33bc992e789
panji_agustoro <panji.agustoro@solusi247.com>
2015-09-16 16:06:30 +0700

    new upload

---
+++ src/com/Controller/DesignController.java
@@ -0,0 +1,17 @@
+package com.Controller;
+
+import com.View.PanelDesign;
+
+public class DesignController {
+    
+    private final PanelDesign panel;
+
+    public DesignController(PanelDesign panel) {
+        this.panel = panel;
+    }
+    
+    public void getPanel(){
+        
+    }
+    
+}
